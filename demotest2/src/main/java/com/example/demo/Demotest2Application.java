e2168c380392e7e4340d0f8d5fa53bb8a366d776
GraceGu <GraceGu@04022-GGu>
2018-08-28 14:01:11 -0500

    commit 2

---
+++ demotest2/src/main/java/com/example/demo/Demotest2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demotest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demotest2Application.class, args);
+	}
+}
