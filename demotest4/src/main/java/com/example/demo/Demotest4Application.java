51d3b0a202df31b44ddc912616ddac1eda37fb96
GraceGu <GraceGu@04022-GGu>
2018-08-28 14:25:00 -0500

    commit 4

---
+++ demotest4/src/main/java/com/example/demo/Demotest4Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demotest4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demotest4Application.class, args);
+	}
+}
